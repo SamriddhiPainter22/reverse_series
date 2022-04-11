e53374b3f04e0a6b2a65128dc21e5b08298ecb9c
Samriddhi Painter <88041669+SamriddhiPainter22@users.noreply.github.com>
2022-04-11 19:26:56 +0530

    first commit

---
+++ reverse_series.py
@@ -0,0 +1,9 @@
+#initialize array
+arr = [4,20,3,46,5,8,9]
+print("Original array: ")
+for i in range (0,len(arr)):
+    print(arr[1])
+print("Array in reverse order: ")
+
+for i in range(len(arr)-1,-1,-1):
+    print(arr[i])
